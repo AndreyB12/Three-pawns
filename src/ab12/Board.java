48bca30fadf97aa11861648f9823affcd6a246c2
Andrey Butko <andrey.butko@gmail.com>
2016-09-24 23:33:23 +0300

    First commit

---
+++ src/ab12/Board.java
@@ -0,0 +1,17 @@
+package ab12;
+
+import java.io.Serializable;
+
+/**
+ * Created by butkoav on 24.09.2016.
+ */
+public class Board implements Serializable {
+    public static Board game;
+    int[][] board = new int[3][8];
+
+    public void main(String... args) {
+
+    }
+
+
+}
---
+++ src/ab12/Pawn.java
@@ -0,0 +1,15 @@
+package ab12;
+
+/**
+ * Created by butkoav on 24.09.2016.
+ */
+public class Pawn {
+    int x;
+    int y;
+
+    public void draw() {
+
+    }
+
+
+}
